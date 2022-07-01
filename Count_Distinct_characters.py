522a9dcc9e92c0f7e9ac44a802b044672c41330c
NIKHILA-PYLA <97740939+NIKHILA-PYLA@users.noreply.github.com>
2022-07-01 15:45:41 +0530

    a=input() arr=[] for i in a:     if i.isspace():         continue     else:         if i.upper() not in arr and i.lower() not in arr:             arr.append(i) print(len(arr))

---
+++ Count_Distinct_characters.py
@@ -0,0 +1,10 @@
+a=input()
+arr=[]
+for i in a:
+    if i.isspace():
+        continue
+    else:
+        if i.upper() not in arr and i.lower() not in arr:
+            arr.append(i)
+print(len(arr))
+
